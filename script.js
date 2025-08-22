044009747c8afd83c5922b964e94f1f158039f52
Kari Meghana <karimeghana02@gmail.com>
2025-08-22 10:47:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+function showTime() {
+    const date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("timer").innerText = time;
+    setTimeout(showTime, 1000);
+}
+
+showTime();
